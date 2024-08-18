0234fe4250a9b2cace4eafa56374c5bebe320467
Darwin Velez <darwin.velez@priverion.com>
2024-08-17 21:05:58 -0500

    playground: reading latest deployed contract

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,16 +1,15 @@
 from ape import accounts, project
-
+import json
 
 def deploy_simple_storage():
-    #my_test_account = accounts.load("test-sepolia")
-    my_local_account = accounts.test_accounts[0]
+    my_local_account = accounts.load("test-sepolia")
+    # my_local_account = accounts.test_accounts[0]
     simple_storage_contract = my_local_account.deploy(project.SimpleStorage)
     stored_value = simple_storage_contract.retrieve()
     print(f"stored_value: {stored_value}")
     tx = simple_storage_contract.store(15, sender=my_local_account)
     print(f"xt: {tx}")
     print(f"retrieve: {simple_storage_contract.retrieve()}")
-
 
 
 def main():
